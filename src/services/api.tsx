94d175601093ad6a017078ba9765cdcc708cd26b
Thang <thangnus@gmail.com>
2021-01-22 20:50:51 +1300

    Add real cover letter snippets

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -18,37 +18,55 @@ export const word = (n: number) => faker.lorem.words(n);
 const snippets: ISnippet[] = [
   {
     id: faker.random.uuid(),
-    body: faker.lorem.sentence(10),
+    body: `Thang Pham 
+    21 Whitaker Place, 
+    Grafton 1010, Auckland
+    0210 868 2380 
+    thangnus@gmail.com
+    5 Jan 2021
+    Dear Hiring Manager,
+    I’m writing to apply for the Graduate Developer position, as advertised on SEEK.  
+    `,
     lane: LaneType.Snippet,
     tags: [`${word(1)}`, `${word(1)}`],
   },
   {
     id: faker.random.uuid(),
-    body: faker.lorem.sentence(10),
+    body: `Thank you for considering my application. I look forward to hearing from you.
+    Yours sincerely
+    Thang Pham`,
     lane: LaneType.Snippet,
     tags: [`${word(1)}`, `${word(1)}`],
   },
   {
     id: faker.random.uuid(),
-    body: faker.lorem.sentence(10),
+    body: `All in all, I believe I can quickly integrate and become a productive team member. I’m also very excited at the chance to craft digital solutions for different sectors with diverse needs.`,
     lane: LaneType.Draft,
     tags: [`${word(1)}`, `${word(1)}`],
   },
   {
     id: faker.random.uuid(),
-    body: faker.lorem.sentence(10),
+    body: `I’ve worked with teammates from diverse backgrounds using agile practices like sprint planning and daily standups. I can use Git to do merges, rebasing, conflict resolution, etc. I also use Bitbucket, Jira, and Confluence on a daily basis.
+    `,
     lane: LaneType.Draft,
     tags: [`${word(1)}`, `${word(1)}`],
   },
   {
     id: faker.random.uuid(),
-    body: faker.lorem.sentence(10),
+    body: `As an intern junior developer at Mentemia’s agile team since November, I have deployed two features to production: mobile app notifications around subscription expiry and changes, and an API endpoint to automatically activate premium subscription when customers link their account.
+    `,
     lane: LaneType.Snippet,
     tags: [`${word(1)}`, `${word(1)}`],
   },
   {
     id: faker.random.uuid(),
-    body: faker.lorem.sentence(10),
+    body: `I have personal projects using React, Redux, React-Router and other common libraries, have a fair grasp of basic design patterns in React like Container-Component and Higher-Order Component, and can confidently use React’s standard hooks or write custom ones.`,
+    lane: LaneType.Draft,
+    tags: [`${word(1)}`, `${word(1)}`],
+  },
+  {
+    id: faker.random.uuid(),
+    body: `I have developed JavaScript applications ever since I started programming, and am comfortable with Node’s event-driven architecture, Javascript’s OOP and functional design patterns, ES6/Typescript and Express.`,
     lane: LaneType.Draft,
     tags: [`${word(1)}`, `${word(1)}`],
   },
